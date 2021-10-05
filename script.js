2052a8268d2b78d315072d923365c2ec2c141df0
DeveloperBuilder <35078547+DeveloperBuilder@users.noreply.github.com>
2021-10-05 15:49:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let age = 18;
+let isfemale = true;
+let driverStatus = "bob";
+
+if (age >= 18) {
+    console.log("U  mag naar binnen, u bent ouder 18 jaar of ouder");
+} else {
+    console.log("U mag niet naar binnen, u bent jonger dan 18 jaar");
+}
+
+if (isfemale) {
+    console.log("Vandaag is ladiesnight");
+} else {
+    console.log("Helaas vandaag is geen ladiesnight, tot de volgende keer")
+}
+
+if (driverStatus === "bob") {
+    console.log("u mag rijden");
+} else {
+    console.log("u mag niet rijden");
+}
+
+
